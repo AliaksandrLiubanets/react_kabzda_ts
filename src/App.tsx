6ce1d07bb46f6fcba1ca528ccd5eb36d12e14154
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-10-27 10:26:47 +0300

    refactor delete Toggle component and create directoryes for old components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import AppTitle from "./Components/AppTitle";
-import Rating from "./Components/Rating";
-import Accordion from "./Components/Accordion";
+import AppTitle from "./Components/AppTitle/AppTitle";
+import Rating from "./Components/Rating/Rating";
+import Accordion from "./Components/Accordion/Accordion";
 import {OnOff, } from './Components/onOff/OnOff'
 
 function App() {
   console.log('App rendering')
   return <div>
-    {/*<AppTitle title={'This is App Component'}/>*/}
-    {/*<AppTitle title={'My Friends'}/>*/}
+    <AppTitle title={'This is App Component'}/>
+    <AppTitle title={'My Friends'}/>
     <Accordion title={'Menu'} collapsed={true}/>
     <Accordion title={'Users'} collapsed={false}/>
     <Rating stars={0}/>
@@ -17,7 +17,7 @@ function App() {
     <Rating stars={3}/>
     <Rating stars={4}/>
     <Rating stars={5}/>
-    {/*<Toggle isToggle={false}/>*/}
+    <OnOff  />
     <OnOff  />
   </div>
 }
--- src/Components/onOff/OnOff.tsx
+++ src/Components/onOff/OnOff.tsx
@@ -5,22 +5,6 @@ type OnOffPropsType = {
     // isToggle: boolean
 }
 
-// export function Toggle(props: OnOffPropsType) {
-//     if(props.isToggle) {
-//         return <div className={s.onOff}>
-//             <div className={s.on}>on</div>
-//             <div className={`${s.empty}`}>off</div>
-//             <div className={s.green_circle}></div>
-//         </div>
-//     } else {
-//         return <div className={s.onOff}>
-//             <div className={s.empty}>on</div>
-//             <div className={s.off}>off</div>
-//             <div className={s.red_circle}></div>
-//         </div>
-//     }
-// }
-
 export function OnOff() {
 
     let [isToggle, setToggle] = useState(false)
