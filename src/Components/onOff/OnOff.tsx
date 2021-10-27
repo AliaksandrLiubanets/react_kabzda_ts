2041b664d64adc68fe355a4f00b74889bc14566c
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-10-27 09:57:16 +0300

    refactor setToggle - pass true or false enstead of isToggle = true | false

--- src/Components/onOff/OnOff.tsx
+++ src/Components/onOff/OnOff.tsx
@@ -26,8 +26,8 @@ export function OnOff() {
     let [isToggle, setToggle] = useState(false)
 
         return <div className={s.onOff}>
-            <div className={isToggle ? s.on : s.empty} onClick={() => setToggle(isToggle = true)}>on</div>
-            <div className={isToggle ? s.empty : s.off} onClick={() => setToggle(isToggle = false)}>off</div>
+            <div className={isToggle ? s.on : s.empty} onClick={() => setToggle(true)}>on</div>
+            <div className={isToggle ? s.empty : s.off} onClick={() => setToggle(false)}>off</div>
             <div className={isToggle ? s.green_circle : s.red_circle}></div>
         </div>
 }
