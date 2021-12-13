528ed60344dd0957106573caf7d6c7f5a9b886ef
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-12-13 10:32:40 +0300

    refactor Accordion.stories.tsx: add setCollapsed in Collapsed.args

--- src/Components/Accordion/Accordion.stories.tsx
+++ src/Components/Accordion/Accordion.stories.tsx
@@ -13,22 +13,17 @@ export const Uncollapsed = Template.bind({});
 Uncollapsed.args = {
     collapsed: false,
     title: 'Menu',
+    setCollapsed: () => console.log('hello')
 };
 
 export const Collapsed = Template.bind({});
 Collapsed.args = {
     collapsed: true,
     title: 'Menu',
+    setCollapsed: () => console.log('hello')
 };
 
 export const ChangeMode: ComponentStory<typeof Accordion> = (args) => {
     const [collapsed, setCollads] = useState<boolean>(false)
     return <Accordion collapsed={collapsed} setCollapsed={setCollads} title={'Menu'}/>
 }
-
-// export const ChangingOnOff = ChangeMode.bind({})
-// ChangingOnOff.args = {
-//     // isToggle: false,
-//     // setIsToggle: () => { setIsToggle()}
-//
-// }
