de5eb352dc5509ca7b57f6b8197b0b6a10bf641c
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-12-23 21:07:02 +0300

    Create Time.stories.tsx

---
+++ src/stories/Time.stories.tsx
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react'
+
+export default {
+    title: 'Time'
+}
+
+export const Time = () => {
+    console.log('SetIntervalExample2')
+
+    const [count, setCount] = useState(1)
+
+    useEffect(() => {
+        console.log('useEffect')
+        setInterval(() => {
+            console.log('tick ' + count)
+            setCount((state => state + 1))
+        }, 1000)
+    }, [])
+
+    return <>
+        Hello! {count}
+    </>
+}
+
