4f961c8f8b43a4490344cc0c9179639a2235f720
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-12-23 12:59:23 +0300

    Create SelectMemo.stories.tsx

--- src/Components/Select/SelectMemo.stories.tsx
+++ src/Components/Select/SelectMemo.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useMemo, useState} from 'react'
 import {ComponentMeta, ComponentStory} from '@storybook/react'
 import Select from './Select'
 
@@ -8,6 +8,7 @@ export default {
 
 
 export const FilteredPopulation: ComponentStory<typeof Select> = () => {
+    console.log('FilteredPopulationComponent')
     const [value, setValue] = useState('2')
     const [items, setItems] = useState([
         {id: 1, title: 'Dimych', value: '1', city: 'Minsk', population: 2},
@@ -16,16 +17,33 @@ export const FilteredPopulation: ComponentStory<typeof Select> = () => {
         {id: 4, title: 'Sveta', value: '4', city: 'Grodno', population: 0.25},
         {id: 5, title: 'Katya', value: '5', city: 'Novopolock', population: 0.11},
         {id: 6, title: 'Roma', value: '6', city: 'M.G.', population: 0.03},
-        {id: 7, title: 'Viktor', value: '7', city: 'Vitebsk.', population: 0.31}
+        {id: 7, title: 'Viktor', value: '7', city: 'Vitebsk', population: 0.31}
     ])
 
-    const filteredItems = items.filter(item => item.population > 0.3)
+    const filteredItems = items.filter(item => {
+        console.log('filteredItems')
+        // console.log(item)
+        return item.population > 0.3
+    })
 
-    const filteredItem = items.find(item => item.value === value)
+    // const filteredItem = items.find(item => {
+    //     if(item.value === value) {
+    //         console.log('name')
+    //         console.log(item.value)
+    //         return item
+    //     }
+    // })
 
-    const listOfItems = filteredItems.map(item => <div key={item.id}>
-        <div>city: {item.city}. population: {item.population}.</div>
-    </div>)
+    const listOfItems = useMemo(() => {
+       return items.filter(item => item.population > 0.3)
+            .map(item => {
+                console.log('filteredCity')
+                console.log(item.city)
+                return <div key={item.id}>
+                    <div>city: {item.city}. population: {item.population}.</div>
+                </div>
+            })
+    }, [items])
 
     return <>
         <Select value={value}
@@ -35,7 +53,7 @@ export const FilteredPopulation: ComponentStory<typeof Select> = () => {
             {listOfItems}
         </div>
         <hr/>
-        <div>name according value {value}: {filteredItem ? filteredItem.title : 'No name'}</div>
+        {/*<div>name according value {value}: {filteredItem ? filteredItem.title : 'No name'}</div>*/}
     </>
 }
 
