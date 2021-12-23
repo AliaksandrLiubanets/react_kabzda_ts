b23c4f4ba93bde995bd811b6a243fd97711a5835
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-12-23 18:11:10 +0300

    Create UseEffect.stories.tsx

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from 'react'
+
+export default {
+    title: 'useEffect'
+}
+
+export const Example1 = () => {
+    console.log('Example1')
+
+    const [count, setCount] = useState(1)
+    const [fake, setFake] = useState(1)
+    document.title = 'fsdds'
+
+    useEffect(() => {
+        console.log("sueEffect every render")
+        document.title = count.toString()
+    })
+
+    useEffect(() => {
+        console.log("sueEffect first render")
+        document.title = count.toString()
+    }, [])
+
+    useEffect(() => {
+        console.log("sueEffect first render and every count changing")
+        document.title = count.toString()
+    }, [count])
+
+    const changer = (state: number) => {
+        // console.log('changer')
+        return state + 1
+    }
+
+    return <>
+        <div>Hello! {count}
+            <button onClick={() => setCount(changer)}>+</button>
+        </div>
+        <div>fake: {fake}
+            <button onClick={() => setFake(fake + 1)}>+</button>
+        </div>
+
+    </>
+}
+
+
