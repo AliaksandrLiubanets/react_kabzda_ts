8db9ef090cbb6dc16a63b11b42f5b45c737a08b8
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-12-23 18:40:59 +0300

    Create SetTimeoutExample in UseEffect.stories.tsx

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -27,7 +27,6 @@ export const Example1 = () => {
     }, [count])
 
     const changer = (state: number) => {
-        // console.log('changer')
         return state + 1
     }
 
@@ -42,4 +41,32 @@ export const Example1 = () => {
     </>
 }
 
+export const SetTimeoutExample = () => {
+    console.log('Example1')
+
+    const [count, setCount] = useState(1)
+    const [fake, setFake] = useState(1)
+    document.title = 'fsdds'
+
+    useEffect(() => {
+        console.log("sueEffect every render")
+        document.title = count.toString()
+    })
+
+
+    const changer = (state: number) => {
+        // console.log('changer')
+        return state + 1
+    }
+
+    return <>
+        <div>Hello! {count}
+            <button onClick={() => setCount(changer)}>+</button>
+        </div>
+        <div>fake: {fake}
+            <button onClick={() => setFake((state) => state + 1)}>+</button>
+        </div>
+
+    </>
+}
 
