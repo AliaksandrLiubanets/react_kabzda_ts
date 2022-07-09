8e8394746672cb4e0c00bdae806a197603d853ab
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-07-09 23:12:45 +0300

    Create GitHub component

---
+++ src/Components/GitHub/GitHub.tsx
@@ -0,0 +1,20 @@
+import s from './GitHub.module.css'
+
+export const GitHub = () => {
+    return <div className={s.container}>
+        <div>
+            <div>
+                <input/>
+            </div>
+            <ul>
+                {['Dimych', 'Artem']
+                    .map(u => <li onClick={() => document.title = u}>{u}</li>)
+                }
+            </ul>
+        </div>
+        <div>
+            <h2>UserName</h2>
+            <div>Details</div>
+        </div>
+    </div>
+}
