c1b5528a807129d688cbc03cd26b19583515c11d
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-07-09 23:34:44 +0300

    Add useEffect to GitHub

--- src/Components/GitHub/GitHub.tsx
+++ src/Components/GitHub/GitHub.tsx
@@ -1,20 +1,29 @@
 import s from './GitHub.module.css'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
 export const GitHub = () => {
     const [selectedUser, setSelectedUser] = useState<string | null>(null)
+
+    useEffect(() => {
+        if (selectedUser) {
+            document.title = selectedUser
+        }
+
+    }, [selectedUser])
+
     return <div className={s.container}>
         <div>
             <div>
-                <input placeholder={'search'}/><button>find</button>
+                <input placeholder={'search'}/>
+                <button>find</button>
             </div>
             <ul>
                 {['Dimych', 'Artem']
                     .map(u => <li className={u === selectedUser ? s.selected : ''}
-                        onClick={() => {
-                            setSelectedUser(u)
-                            document.title = u
-                        }}>{u}</li>)
+                                  onClick={() => {
+                                      setSelectedUser(u)
+                                      // document.title = u
+                                  }}>{u}</li>)
                 }
             </ul>
         </div>
