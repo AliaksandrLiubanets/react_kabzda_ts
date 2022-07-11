75d5d1def57673b3849ea08a07aee24ae571e075
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-07-11 22:02:42 +0300

    Create Header.tsx component

---
+++ src/Components/GitHub/Header/Header.tsx
@@ -0,0 +1,31 @@
+import {FC, useEffect, useState} from 'react'
+import axios from 'axios'
+import {SearchResult, SearchUserType} from '../GitHub'
+
+type HeaderProps = {
+    // tempSearch: string
+    // setTempSearch: (value: string) => void
+    // setSearchTerm: (value: string) => void
+    setUsers: (users: SearchUserType[]) => void
+}
+
+export const Header: FC<HeaderProps> = ({setUsers}) => {
+    const [tempSearch, setTempSearch] = useState<string>('it-kamasutra')
+    const [searchTerm, setSearchTerm] = useState<string>('it-kamasutra')
+
+    useEffect(() => {
+        axios.get<SearchResult>('https://api.github.com/search/users?q=' + tempSearch)
+            .then(response => setUsers(response.data.items))
+    }, [searchTerm])
+
+    return <div>
+        <input placeholder={'search'}
+               value={tempSearch}
+               onChange={(e) => setTempSearch(e.currentTarget.value)}
+        />
+        <button onClick={() => {
+            setSearchTerm(tempSearch)
+        }}>find
+        </button>
+    </div>
+}
