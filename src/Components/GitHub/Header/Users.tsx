e3bf84a434e574dbdc4822bf8dd23a62744abefc
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-07-11 22:03:21 +0300

    Create User.tsx component

---
+++ src/Components/GitHub/Header/Users.tsx
@@ -0,0 +1,29 @@
+import {FC, useEffect} from 'react'
+import s from '../GitHub.module.css'
+import {SearchUserType} from '../GitHub'
+
+type UsersProps = {
+    users: SearchUserType[]
+    selectedUser: SearchUserType | null
+    setSelectedUser: (user: SearchUserType) => void
+}
+
+export const Users: FC<UsersProps> = ({users, selectedUser, setSelectedUser}) => {
+
+    useEffect(() => {
+        if (selectedUser) {
+            document.title = selectedUser.login
+        }
+    }, [selectedUser])
+
+    return <>
+        <ul>
+            {users.map(u => <li key={u.id}
+                                className={`${s.user} ${u === selectedUser ? s.selected : ''}`}
+                                onClick={() => {
+                                    setSelectedUser(u)
+                                }}>{u.login}</li>)
+            }
+        </ul>
+    </>
+}
