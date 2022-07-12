71ac703bcf231d60354e5baee7a372da71a543ba
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-07-12 15:17:07 +0300

    Create Timer component

---
+++ src/Components/GitHub/Timer/Timer.tsx
@@ -0,0 +1,30 @@
+import {FC, useEffect, useState} from 'react'
+import {SearchUserType} from '../GitHub'
+
+type TimerType = {
+    initialDigit: number
+    selectedUser: SearchUserType | null
+    setIsTimeIsUp: (isTimeIsUp: boolean) => void
+}
+
+export const Timer: FC<TimerType> = ({initialDigit, selectedUser, setIsTimeIsUp}) => {
+    const [digit, setDigit] = useState<number>(initialDigit)
+
+    useEffect(() => {
+        setDigit(initialDigit)
+    }, [selectedUser])
+
+    useEffect(() => {
+        if (digit === 10) {
+            setIsTimeIsUp(false)
+        }
+        const id = setInterval(() => {
+            setDigit(digit - 1)
+        }, 1000)
+        return () => {
+            clearInterval(id)
+        }
+    }, [digit])
+
+    return <div>{digit}</div>
+}
